bd72c23f5e7a65017b43123b9bb1943a75a05401
cassaram09 <cassaram09@gmail.com>
2017-02-25 11:37:45 -0500

    add fix for resizing slide heights"

--- dist/ionSlideBoxTabs.js
+++ dist/ionSlideBoxTabs.js
@@ -6,8 +6,8 @@ function ionSlideBoxTabs(){
     template: [
       '<div class="slide-tabs">',
         '<ul class="slide-tab-list">',
-          '<li ng-click="selectTab($index)" class="label" ng-repeat="tab in tabs track by $index">',
-            '<div ng-bind="tab"></div>',
+          '<li ng-click="selectTab($index)" class="label" ng-repeat="tab in tabs track by $index" ng-style="tabWidth">',
+            '<div><span ng-bind="tab"></span></div>',
           '</li>',
         '</ul>',
         '<div class="indicator-wrapper">',
@@ -20,21 +20,14 @@ function ionSlideBoxTabs(){
     controller: function($scope, $timeout, $ionicSlideBoxDelegate, $ionicScrollDelegate, $ionicGesture) {
 
       $scope.tabs = [];
+      $scope.tabWidth = {"width": '0%'}
+      $scope.slideHeights = {}
+      $scope.currentSlideIndex = 0;
+      $scope.indicator = angular.element('#slide-tab-indicator')
+      $scope.transclude = angular.element('#slide-box-content')
 
-      var indicator = angular.element('#slide-tab-indicator')
-      var transclude = angular.element('#slide-box-content')
-      transclude.css("height", '90%')
-
-      $scope.snapToPosition = function(){
-        var index = $ionicSlideBoxDelegate.currentIndex();
-        moveIndicator(index)
-      }
-
-      $scope.selectTab = function(index) {
-        scrollTopAndResize()
-        $ionicSlideBoxDelegate.slide(index)
-        moveIndicator(index)
-      }
+      $scope.transclude.css("height", '90%')
+      $scope.minSlideHeight = getMinSlideHeight();
 
       $scope.$on('slideChanged', function() {
         var index = $ionicSlideBoxDelegate.currentIndex();
@@ -42,15 +35,46 @@ function ionSlideBoxTabs(){
         moveIndicator(index)
       });
 
+      $scope.snapToPosition = function(){
+        var index = $ionicSlideBoxDelegate.currentIndex();
+        moveIndicator(index)
+      }
+
+      $scope.selectTab = function(index) {
+        $ionicSlideBoxDelegate.slide(index)
+        moveIndicator(index)
+        scrollTopAndResize(index)
+      }
+
+      $scope.onGesture = function(gesture) {
+        move(gesture)
+      }
+
+      function getSlideHeight(index) {
+        // get the current slide's height from our hash
+        if ( $scope.slideHeights[index] ) {
+          return $scope.slideHeights[index]
+        } else {
+          // runs on the first move of the slider - collects all slide heights
+          var slides = angular.element('ion-slide').length
+          $scope.slidesContainer = angular.element('.slider-slides')
+          for ( var i = 0; i < slides; i++) {
+            var slideHeight = angular.element("[slide-tab-label='" + $scope.tabs[i] + "']").height() 
+            var height = Math.max(slideHeight, $scope.minSlidHeight);
+            $scope.slideHeights[i] = height
+          }
+          return $scope.slideHeights[index]
+        }  
+      }
+
       function move(gesture) {
         var index = $ionicSlideBoxDelegate.currentIndex() + 1
-        if ( ( index == 1 && gesture == 'right' ) || ( index == 3 && gesture == 'left' ) ) {
+        if ( ( index == 1 && gesture == 'right' ) || ( index == $scope.tabs.length && gesture == 'left' ) ) {
           return
         }
-
         var slide = angular.element(".slider-slide:nth-child(" + index + ")" )
         var leftOffset = slide.offset().left
-        var width = slide.width()
+        var width = angular.element(window).width()
         var position = (Math.abs(leftOffset) / width) * 100;
         
         if ( position > index * 100) {
@@ -59,35 +83,32 @@ function ionSlideBoxTabs(){
 
         if ( gesture == 'right') {
           position = ( (index - 1) * 100 ) - position
-          indicator.css("transform", "translate("+ position + "%" + ",0%)")
+          $scope.indicator.css("transform", "translate("+ position + "%" + ",0%)")
         } else {
           position = position + ( (index - 1) * 100)
-          indicator.css("transform", "translate("+ position + "%" + ",0%)")
+          $scope.indicator.css("transform", "translate("+ position + "%" + ",0%)")
         }
         return true;
       }
 
-      $scope.onGesture = function(gesture) {
-        move(gesture)
-      }
-
       function moveIndicator(index) {
         var position = (index * 100) + "%"
-        indicator.css("transform", "translate("+ position + ",0%)")
+        $scope.indicator.css("transform", "translate("+ position + ",0%)")
       }
 
-      function scrollTopAndResize() {
+      function scrollTopAndResize(index) {
         $ionicScrollDelegate.scrollTop(true);
-         $timeout( function() {
-          $ionicScrollDelegate.resize();
-        }, 50);
-      }
+        var height = getSlideHeight(index)
+        $scope.slidesContainer.css('height', height)
+      } 
 
-      angular.element(document).ready(function () {
-        var width = 100 / $scope.tabs.length
-        angular.element('li.label').css('width', width +  "%")
-        indicator.css('width', width +  "%")
-      });
+      function getMinSlideHeight(){
+        var tabHeight = angular.element("ion-tab").height()
+        var windowHeight = angular.element(window).height()
+        var topOffset = $scope.transclude.offset().top
+        var height = ( windowHeight - topOffset - tabHeight )
+        return height;
+      }
     
     }
   }
@@ -100,7 +121,14 @@ ionSlideBoxTabsModule
 function slideTabLabel(){
   return {
     link: function ($scope, $element, $attrs, $parent) { 
-      $scope.$parent.$parent.$parent.tabs.push($attrs.slideTabLabel);
+      var tabs = $scope.$parent.$parent.$parent.tabs;
+      tabs.push($attrs.slideTabLabel);
+
+      var tabWidth = $scope.$parent.$parent.$parent.tabWidth
+      var width = 100 / tabs.length
+      tabWidth.width = width + '%'
+
+      $scope.indicator.css('width', width +  "%")
     }
   }
 }
